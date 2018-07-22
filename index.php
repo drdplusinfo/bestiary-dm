099f0e2e2424dea77eecafdc418c322e9a201016
Jaroslav Týc <mail@jaroslavtyc.com>
2018-07-22 16:36:11 +0200

    Version can be changed

--- index.php
+++ index.php
@@ -1,17 +1,5 @@
 <?php
-$masterDocumentRoot = $masterDocumentRoot ?? (PHP_SAPI !== 'cli' ? \rtrim(\dirname($_SERVER['SCRIPT_FILENAME']), '\/') : \getcwd());
-$documentRoot = $documentRoot ?? $masterDocumentRoot;
-$latestVersion = $latestVersion ?? '1.0';
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-$dirs = new \DrdPlus\RulesSkeleton\Dirs($masterDocumentRoot, $documentRoot);
-
-$controller = $controller ?? new \DrdPlus\RulesSkeleton\RulesController(
-        'UA-121206931-5',
-        \DrdPlus\RulesSkeleton\HtmlHelper::createFromGlobals($dirs),
-        $dirs
-    );
-$controller->setFreeAccess();
+$googleAnalyticsId = 'UA-121206931-3';
+$hasFreeAccess = true;
 
 require __DIR__ . '/vendor/drd-plus/rules-skeleton/index.php';
