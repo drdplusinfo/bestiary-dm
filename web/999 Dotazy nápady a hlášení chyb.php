6f51996c1846fcc0ecdf7c5bd4229f0f930da0e4
Jaroslav Týc <mail@jaroslavtyc.com>
2019-01-18 16:47:33 +0100

    Underlynig skeleton updated

---
+++ web/999 Dotazy nápady a hlášení chyb.php
@@ -0,0 +1,36 @@
+<h2>Dotazy, nápady a hlášení chyb</h2>
+
+<h3>Dotazy a odpovědi</h3>
+<div>
+  Pokud si nejsi s něčím jistý, v hlavě ti hlodá otázka, na jazyk se ti dere "no jo, ale co když...", podívej se na
+  <a href="https://rpgforum.cz/forum/viewforum.php?f=238">rpgforum</a>. Lidí, co ti pomůžou, je tam dost. A taky je
+  tam spousta otázek už rozebraných. Hrrr na ně!
+</div>
+
+<h3>Kam s nápady</h3>
+<div>
+  Něco ti v pravidlech chybí? Nebo přebývá? Napadl tě zlepšovák? Popiš nám tvůj nápad v <a
+      href="https://rpgforum.cz/forum/viewtopic.php?f=238&t=14870">sekci
+    na rpgforu</a>.
+  <p>Za každý podnět děkujeme!</p>
+</div>
+
+<h3>Ha, chyba!</h3>
+<div>
+  <div class="introduction">
+    Byla nádherná, téměř dokonalá. I přes svou mohutnost se její tělo vznášelo ve večerním vánku ladně, ba nadpozemsky,
+    trojice křídel vířila v
+    přesném sledu, vykreslujíc v červáncích vlnivou nádheru, pětihrané hlavy se komíhaly, zvolna, rytmicky a
+    přesto zlověstně, mocné pařáty se jen občas a jaksi mazlivě dotkly vzpřímených stébel Bojínku.
+    <p>Měla jen jedinou, zato nepřekonatelnou chybu. Nikdo na ní nevěřil.</p>
+  </div>
+  <p><?= /** @noinspection PhpUndefinedVariableInspection */
+      $debugContacts ?>
+    My už tu potvoru vrátíme nohama na zem.</p>
+  <p>Každý střípek nám pomůže zlepšit pravidla a zážitek z DrD+. Děkujeme Ti.</p>
+</div>
+
+<h2>Konec</h2>
+<div>
+  A to je konec... Co? Kdepak, to je začátek. Vzhůru za dobrodružstvím!
+</div>
